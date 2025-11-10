d1fba633384deac88b11ff3165d090272c424324
SantiVVC <santiago.v.v.c1077@gmail.com>
2025-11-10 01:04:59 -0300

    complete la carpeta Juego.js de los modelos, me queda resena para mañana y luego la carpeta routes

---
+++ models/Juego.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const juegoSchema = new mongoose.Schema({
+  titulo: { type: String, required: true },
+  genero: { type: String },
+  completado: { type: Boolean, default: false },
+  horasJugadas: { type: Number, default: 0 },
+  puntuacion: { type: Number, default: 0 },
+  portada: { type: String } // URL o link a la imagen del juego
+});
+
+module.exports = mongoose.model('Juego', juegoSchema);
